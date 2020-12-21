a345272befe4623258c41f005b1b06cd27090eec
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-12-22 00:05:35 +0200

    Express Server Setup

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,1 +1,11 @@
-console.log("Server not Included");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>ALTCE</h1>");
+});
+
+app.listen(PORT, () => console.log(`Server Starting on Port: ${PORT}`));
