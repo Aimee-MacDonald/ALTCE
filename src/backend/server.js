64c7ce3acd8ea2971c6ccbc8e9c09365c1de65eb
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-12-22 18:38:38 +0200

    Basic Frontend Setup

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 
 const PORT = process.env.PORT || 8000;
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "../frontend/src/views"));
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>ALTCE</h1>");
+  res.status(200).render("index");
 });
 
 app.listen(PORT, () => console.log(`Server Starting on Port: ${PORT}`));
