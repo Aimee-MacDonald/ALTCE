05f821bd0266b2375b5f2f48b1dfc9e9968ff3eb
Aimee MacDonald <aimeelmacdonald@gmail.com>
2020-12-24 10:41:09 +0200

    Sass setup

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "normalize.css";
+import "./main.sass";
+
 import ListManager from "./components/ListManager/ListManager";
 
 ReactDOM.render(<ListManager />, document.getElementById("ALTCE"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require("path");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: {
     index: path.join(__dirname, "src/frontend/src/main.js")
   },
   output: {
-    path: path.join(__dirname, "src/frontend/dist/scripts"),
+    path: path.join(__dirname, "src/frontend/dist"),
     filename: "[name].bundle.js"
   },
   module: {
@@ -22,8 +22,24 @@ module.exports = {
             ]
           }
         }
+      }, {
+        test: /\.(sa|c)ss$/,
+        use: [
+          {loader: MiniCssExtractPlugin.loader},
+          {loader: "css-loader"},
+          {loader: "postcss-loader"},
+          {
+            loader: "sass-loader",
+            options: {implementation: require("sass")}
+          }
+        ]
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].bundle.css"
+    })
+  ],
   mode: "development"
 }
